3ce27ea993a7d2db0bbb64271cde2707a5a3f82b
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-07-04 21:49:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let freq = new Array(26).fill(0);
+	// pre-compute
+	for(let i=0; i<str.length; i++){
+		let idx = str.charCodeAt(i) - 'a'.charCodeAt(0);
+		freq[idx] += 1;
+	}
+
+	// fetch
+	for(let i=0; i<str.length; i++){
+	    let idx = str.charCodeAt(i) - 'a'.charCodeAt(0);
+		if(freq[idx] === 1){
+			return str[i];
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
